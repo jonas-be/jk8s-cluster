d790a6c73fe7c0f350ebd5d14b65df3df9767990
jonas-be <90411737+jonas-be@users.noreply.github.com>
2023-12-28 01:25:23 +0100

    Implement install containerd

--- bootstrap/node-init.sh
+++ bootstrap/node-init.sh
@@ -17,7 +17,29 @@ apt update
 apt upgrade -y
 
 # Install containerd
-echo "Comming soon"
+cat <<EOF | sudo tee /etc/modules-load.d/containerd.conf
+overlay 
+br_netfilter
+EOF
+sudo modprobe overlay
+sudo modprobe br_netfilter
+cat <<EOF | sudo tee /etc/sysctl.d/99-kubernetes-k8s.conf
+net.bridge.bridge-nf-call-iptables = 1
+net.ipv4.ip_forward = 1 
+net.bridge.bridge-nf-call-ip6tables = 1 
+EOF
+
+sudo sysctl --system
+
+sudo apt update
+sudo apt -y install containerd
+
+containerd config default | sudo tee /etc/containerd/config.toml >/dev/null 2>&1
+
+sed -i 's/SystemdCgroup = false/SystemdCgroup = true/g' /etc/containerd/config.toml
+
+sudo systemctl restart containerd
+sudo systemctl enable containerd
 
 # Install kubelet, kubeadm, kubectl
 apt install -y apt-transport-https ca-certificates curl gpg
