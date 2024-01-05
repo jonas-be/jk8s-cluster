8cc48031d5b03c9753c755f8219d1b90142200a9
jonas-be <90411737+jonas-be@users.noreply.github.com>
2024-01-05 22:55:34 +0100

    Fix traefik deployment

--- treafik/deploy.sh
+++ treafik/deploy.sh
@@ -2,6 +2,6 @@
 
 helm repo add traefik https://traefik.github.io/charts
 helm repo update
-helm install traefik traefik/traefik
 
+kubectl create namespace traefik
 helm install --namespace=traefik --values values.yaml traefik traefik/traefik
