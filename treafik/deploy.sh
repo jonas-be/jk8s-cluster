09a61efd9af4b8cbd6ed4192ff547c639686ed48
jonas-be <90411737+jonas-be@users.noreply.github.com>
2024-01-05 23:26:25 +0100

    Fix Traefik deployment

--- treafik/deploy.sh
+++ treafik/deploy.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
+# Treafik
 helm repo add traefik https://traefik.github.io/charts
 helm repo update
 
 kubectl create namespace traefik
 helm install --namespace=traefik --values values.yaml traefik traefik/traefik
+
+
+# Cert Manager
+kubectl apply -f https://github.com/jetstack/cert-manager/releases/download/v1.13.3/cert-manager.yaml
+
+
+# Cluster Issuer
+kubectl apply -f letsencrypt-issuer.yaml
