ff70c7507adc4f49ddb9a3c2386a90fd76fe44be
jonas-be <90411737+jonas-be@users.noreply.github.com>
2024-01-13 01:30:17 +0100

    Clean up

--- ceph/deploy.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-helm repo add rook-release https://charts.rook.io/release
-helm install --create-namespace --namespace rook-ceph rook-ceph rook-release/rook-ceph -f values.yaml
-
-# After ceph cluster is ready do:
-# kubectl apply -f cluster.yaml
